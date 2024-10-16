405c87cb87620c37e51fb0423aedaca74ad1e65d
FMHJ97 <francisco-h-j@hotmail.com>
2024-10-16 23:43:51 +0200

    feat(tema3.Ejercicio_Repaso_03): mostrarStock finished

--- tema3/Apuntes/Ejercicio_Repaso_03/mostrarStock.php
+++ tema3/Apuntes/Ejercicio_Repaso_03/mostrarStock.php
@@ -16,26 +16,53 @@
                 $conex->set_charset("utf8mb4");
             } catch (Exception $ex) {
                 die("ERROR, NO SE HA PODIDO CONECTAR A LA BD.");
-            }
+            }           
         ?>
         
         <div id="encabezado">
             <h1>Mostrar Stock de un Producto en cada una de las Tiendas</h1>
             <form action="" method="POST">
+                Producto:
                 <select name="producto">
                     <?php
                         try {
                             $result = $conex->query("SELECT * FROM producto");
-                            if ($result->num_rows) {
-                                $fila = $result->fetch_object();
-                                
+                            // Recorremos las filas obtenidas de la consulta.
+                            while ($fila = $result->fetch_object()) {
+                                // Marcar como seleccionado el producto que el usuario ha escogido
+                                $selected = ($fila->cod == $selectedItem) ? 'selected' : '';                                
+                                echo "<option value='$fila->cod' $selected>$fila->nombre_corto</option>";
                             }
                         } catch (Exception $ex) {
                             die($ex->getMessage());
                         }
                     ?>
                 </select>
+                <input type="submit" name="show" value="Mostrar Stock">
             </form>
+        </div>
+        
+        <div id="contenido">
+            <?php
+                if (isset($_POST['show'])) {
+                    // Guarda el producto seleccionado si se envía el formulario
+                    $selectedItem = isset($_POST['producto']) ? $_POST['producto'] : '';                     
+                    
+                    // Consulta la tienda y stock por producto
+                    $stock_result = $conex->query(
+                            "SELECT tienda, unidades FROM stock WHERE producto='$_POST[producto]'");
+                    
+                    while ($row = $stock_result->fetch_object()) {
+                        // Consulta el nombre de la tienda según el código de tienda obtenido en el stock
+                        $tienda_result = $conex->query(
+                                "SELECT nombre FROM tienda WHERE cod = '$row->tienda'");
+                        
+                        if ($tienda = $tienda_result->fetch_object()) {
+                            echo "<p>Tienda $tienda->nombre = $row->unidades unidades</p>";
+                        }
+                    }
+                }
+            ?>
         </div>
         
     </body>
