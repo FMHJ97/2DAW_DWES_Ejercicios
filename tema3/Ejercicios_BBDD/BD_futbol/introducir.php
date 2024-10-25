58660d0d409025a8ba22a0bccff7716d81893795
FMHJ97 <francisco-h-j@hotmail.com>
2024-10-25 14:53:44 +0200

    fix(BD-futbol): Ejercicio fixed

--- tema3/Ejercicios_BBDD/BD_futbol/introducir.php
+++ tema3/Ejercicios_BBDD/BD_futbol/introducir.php
@@ -3,6 +3,7 @@
         <title>BD futbol - Introducir datos</title>
     </head>
     <body>
+        
         <?php
         // Redirigimos al index en caso de entrar directamente aquí.
         if (!isset($_GET['id'])) {
@@ -30,6 +31,56 @@
                 $main_flag = true;
             }
         }
+        ?>        
+        
+        <h1>Introducir datos</h1>
+        <form action="" method="POST">
+            <p>DNI: <input type="text" name="dni" value="<?php if ($f_dni) echo $_POST['dni']; ?>">
+                <!--Show error-->
+                <?php if (isset($_POST['insertar']) && !$f_dni) echo "<span style='color:red'>Debe tener 8 números y una letra al final.</span>"; ?>
+            </p>
+            <p>Nombre: <input type="text" name="nombre" value="<?php if ($f_nombre) echo $_POST['nombre']; ?>">
+                <!--Show error-->
+                <?php if (isset($_POST['insertar']) && !$f_nombre) echo "<span style='color:red'>No puede estar vacío. Sólo letras.</span>"; ?>                
+            </p>
+            <p>Dorsal: 
+                <select name="dorsal">
+                    <?php
+                    for ($i = 1; $i <= 11; $i++) {
+                        ?>
+                    <option value="<?php echo $i; ?>" <?php if ($f_dorsal && $_POST['dorsal'] == $i) echo "selected"; ?>><?php echo $i; ?></option>
+                        <?php
+                    }
+                    ?>
+                </select>
+            </p>
+            <p>Posición:
+                <select name="posicion[]" multiple="">
+                    <option value="Portero"
+                            <?php if ($f_posicion && in_array("Portero", $_POST['posicion'])) echo "selected"; ?>>Portero</option>
+                    <option value="Defensa"
+                            <?php if ($f_posicion && in_array("Defensa", $_POST['posicion'])) echo "selected"; ?>>Defensa</option>
+                    <option value="Centrocampista"
+                            <?php if ($f_posicion && in_array("Centrocampista", $_POST['posicion'])) echo "selected"; ?>>Centrocampista</option>
+                    <option value="Delantero"
+                            <?php if ($f_posicion && in_array("Delantero", $_POST['posicion'])) echo "selected"; ?>>Delantero</option>
+                </select>
+                <!-- Show Error -->
+                <?php if (isset($_POST['insertar']) && !$f_posicion) echo "<span style='color:red'>Seleccione una o varias posiciones.</span>"; ?>                    
+            </p>
+            <p>Equipo: <input type="text" name="equipo" value="<?php if ($f_equipo) echo $_POST['equipo']; ?>">
+                <!-- Show Error -->
+                <?php if (isset($_POST['insertar']) && !$f_equipo) echo "<span style='color:red'>No puede estar vacío.</span>"; ?>
+            </p>
+            <p>Número de goles: <input type="text" name="goles" value="<?php if ($f_goles) echo $_POST['goles']; ?>">
+                <!-- Show Error -->
+                <?php if (isset($_POST['insertar']) && !$f_goles) echo "<span style='color:red'>No puede estar vacio. Sólo números.</span>"; ?>
+            </p>
+            <input type="submit" name="insertar" value="Insertar">
+            <a href="index.php"><input type="button" name="inicio" value="Inicio"></a>
+        </form>        
+        
+        <?php
         // En caso de pulsar Insertar y todo validado, insertamos el registro.
         if (isset($_POST['insertar']) && $main_flag) {
             // Inicializamos una conexión a la BD.
@@ -45,66 +96,18 @@
                 $posiciones= implode(",", $_POST['posicion']);
                 
                 $conex->query(
-                        "INSERT INTO jugador VALUES ('$_POST[dni]','$_POST[nombre]',$_POST[dorsal],'$posiciones','$_POST[equipo]',$_POST[goles])");
-
+                        "INSERT INTO jugador VALUES ('$_POST[dni]','$_POST[nombre]',$_POST[dorsal],'$posiciones','$_POST[equipo]',$_POST[goles])");     
+            
+                
             } catch (Exception $ex) {
                 if ($ex->getCode() == 1062) die("ERROR. EXISTE UN REGISTRO CON EL MISMO DNI.");
                 die($ex->getMessage());
             }
             // Llegados a este punto, se ha realizado la inserción.
             // Mostramos el mensaje en el menú principal (index.php).
-            header("Location: index.php?msg=REGISTRO INSERTADO CORRECTAMENTE!");
-            $conex->close();
-        } else {
-            ?>
-            <h1>Introducir datos</h1>
-            <form action="" method="POST">
-                <p>DNI: <input type="text" name="dni" value="<?php if ($f_dni) echo $_POST['dni']; ?>">
-                    <!--Show error-->
-                    <?php if (isset($_POST['insertar']) && !$f_dni) echo "<span style='color:red'>Debe tener 8 números y una letra al final.</span>"; ?>
-                </p>
-                <p>Nombre: <input type="text" name="nombre" value="<?php if ($f_nombre) echo $_POST['nombre']; ?>">
-                    <!--Show error-->
-                    <?php if (isset($_POST['insertar']) && !$f_nombre) echo "<span style='color:red'>No puede estar vacío. Sólo letras.</span>"; ?>                
-                </p>
-                <p>Dorsal: 
-                    <select name="dorsal">
-                        <?php
-                        for ($i = 1; $i <= 11; $i++) {
-                            ?>
-                        <option value="<?php echo $i; ?>" <?php if ($f_dorsal && $_POST['dorsal'] == $i) echo "selected"; ?>><?php echo $i; ?></option>
-                            <?php
-                        }
-                        ?>
-                    </select>
-                </p>
-                <p>Posición:
-                    <select name="posicion[]" multiple="">
-                        <option value="Portero"
-                                <?php if ($f_posicion && in_array("Portero", $_POST['posicion'])) echo "selected"; ?>>Portero</option>
-                        <option value="Defensa"
-                                <?php if ($f_posicion && in_array("Defensa", $_POST['posicion'])) echo "selected"; ?>>Defensa</option>
-                        <option value="Centrocampista"
-                                <?php if ($f_posicion && in_array("Centrocampista", $_POST['posicion'])) echo "selected"; ?>>Centrocampista</option>
-                        <option value="Delantero"
-                                <?php if ($f_posicion && in_array("Delantero", $_POST['posicion'])) echo "selected"; ?>>Delantero</option>
-                    </select>
-                    <!-- Show Error -->
-                    <?php if (isset($_POST['insertar']) && !$f_posicion) echo "<span style='color:red'>Seleccione una o varias posiciones.</span>"; ?>                    
-                </p>
-                <p>Equipo: <input type="text" name="equipo" value="<?php if ($f_equipo) echo $_POST['equipo']; ?>">
-                    <!-- Show Error -->
-                    <?php if (isset($_POST['insertar']) && !$f_equipo) echo "<span style='color:red'>No puede estar vacío.</span>"; ?>
-                </p>
-                <p>Número de goles: <input type="text" name="goles" value="<?php if ($f_goles) echo $_POST['goles']; ?>">
-                    <!-- Show Error -->
-                    <?php if (isset($_POST['insertar']) && !$f_goles) echo "<span style='color:red'>No puede estar vacio. Sólo números.</span>"; ?>
-                </p>
-                <input type="submit" name="insertar" value="Insertar">
-                <a href="index.php"><input type="button" name="inicio" value="Inicio"></a>
-            </form>
-            <?php
+            header("Location: index.php?msg=REGISTRO INSERTADO CORRECTAMENTE!");             
+            $conex->close();             
         }
-        ?>        
+        ?>       
     </body>
 </html>
